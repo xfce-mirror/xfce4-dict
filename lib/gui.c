53573930570fab2ee9bcaed3b06f5644bf026b32
Andre Miranda <andreldm@xfce.org>
2020-10-15 00:22:21 -0300

    Update copyright year

--- lib/gui.c
+++ lib/gui.c
@@ -995,7 +995,7 @@ void dict_gui_about_dialog(GtkWidget *widget, DictData *dd)
 		"destroy-with-parent", TRUE,
 		"authors", authors,
 		"comments", _("A client program to query different dictionaries."),
-		"copyright", _("Copyright \302\251 2006-2016 Xfce Development Team"),
+		"copyright", _("Copyright \302\251 2006-2020 Xfce Development Team"),
 		"website", "https://docs.xfce.org/apps/xfce4-dict/start",
 		"logo", logo,
 		"translator-credits", _("translator-credits"),
